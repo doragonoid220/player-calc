1fababd10d68921088a245edcd94d8f5972c1f11
doragonoid220 <r.ivbc.0120@gmail.com>
2026-05-16 22:01:35 +0900

    Update ranking_app.py

--- ranking_app.py
+++ ranking_app.py
@@ -1,5 +1,29 @@
 import streamlit as st
+from PIL import Image
 
-st.title("ランキング分析アプリ")
+st.set_page_config(
+    page_title="ランキング分析",
+    layout="wide"
+)
 
-st.write("ランキング分析用の別アプリです！")
+st.title("ランキング200位 クラブ分析")
+
+st.write("スクショをアップロードしてください")
+
+uploaded_files = st.file_uploader(
+    "ランキングスクショ",
+    type=["png", "jpg", "jpeg"],
+    accept_multiple_files=True
+)
+
+if uploaded_files:
+    st.success(f"{len(uploaded_files)}枚アップロード")
+
+    for file in uploaded_files:
+        image = Image.open(file)
+
+        st.image(
+            image,
+            caption=file.name,
+            width=300
+        )
